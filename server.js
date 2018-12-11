d137c8033e5ab795afbd30b6eaa06c8bd64f0993
Peter Leung <pleungms@hotmail.com>
2018-12-11 02:46:57 +0000

    test

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var app = express();
 
 // Routes
 app.get('/', function (req, res) {
-  res.send('Hello World! [from testapp]');
+  res.send('Hello World! [from testapp auto]');
 });
 
 // Create the server
